f7f2fb9a13691b9372effe751ae4ac2149b470d0
Ashidq <kopettangerang@gmail.com>
2026-06-25 22:01:49 +0700

    initial commit

--- backend/ai-detector/app/main.py
+++ backend/ai-detector/app/main.py
@@ -7,18 +7,20 @@ app = FastAPI(
     title="AI Payment Detector"
 )
 
+FRONTEND_URL = os.getenv(
+    "FRONTEND_URL",
+    "http://localhost:3000"
+)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "https://jujurlycanteens-2c7cnrhjx-ashids-projects.vercel.app"
+        FRONTEND_URL
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(
     payment_router,
